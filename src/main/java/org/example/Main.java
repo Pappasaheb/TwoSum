3ccef772c663524328399ee964f1c3e3ffdafe1d
Pappasaheb <drajegaikwad333@gmail.com>
2024-10-13 23:07:00 +0530

    added optimized way

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,41 @@
 package org.example;
 
+import java.util.HashMap;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] arr={1,2,3,4,5};
+        int target=5;
+
+        /**
+         * by conventional way
+         */
+        for (int i =0; i <arr.length-1; i++) {
+            for (int j=i+1; j<arr.length;j++){
+                if (arr[i]+arr[j]==target){
+                    System.out.println(i +" and "+ j +" are the indices whose values sum to the target");
+                }
+            }
+        }
+
+        /**
+         * by Optimized way
+         */
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+
+            if (map.containsKey(complement)) {
+                System.out.println(map.get(complement) + " and " + i + " are the indices whose values sum to the target");
+            }
+
+            // Store current element with its index
+            map.put(arr[i], i);
         }
     }
 }
